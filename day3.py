c43fc92c482b484213f99ae934f65d8c7bd2d82e
John Lewin <lewin76@gmail.com>
2023-09-20 13:26:33 -0700

    Reverse unncessary change

--- day3.py
+++ day3.py
@@ -11,23 +11,22 @@ with open(input_data) as my_file:
 
 priorities = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 values = []
-for i in range(1, 53):
-    values.append(i)
+for x in range(1, 53):
+    values.append(x)
 
 translation = {priorities: values for priorities, values in zip(priorities, values)}
 groups_of_3 = []
 group = []
 
-# put elves into groups of 3
-
-for i in range(0, len(rucksacks)):
+# Put into groups of 3
+for x in range(0, len(rucksacks)):
     # Group in intervals of 3
-    if i % 3 == 0:
+    if x % 3 == 0:
         # Append group
         groups_of_3.append(group)
-        group = [rucksacks[i]]
+        group = [rucksacks[x]]
     else:
-        group.append(rucksacks[i])
+        group.append(rucksacks[x])
 
 # Append final group
 if group not in groups_of_3:
@@ -46,13 +45,13 @@ for badges in groups_of_3:
 sub_total = []
 final = []
 
-for i in range(0, len(rucksacks)):
-    if i % 3 == 0 and i != 0:
+for x in range(0, len(rucksacks)):
+    if x % 3 == 0 and x != 0:
         # Append group
         final.append(sub_total)
-        sub_total = [no_dupes[i]]
+        sub_total = [no_dupes[x]]
     else:
-        sub_total.append(no_dupes[i])
+        sub_total.append(no_dupes[x])
 
 # Append final group
 if sub_total not in final:
@@ -61,9 +60,9 @@ if sub_total not in final:
 
 print(final)
 badges = []
-for i in range(0, 1):
+for x in range(0, 1):
     for item in final:
-        for c in item[i]:
+        for c in item[x]:
             for char in c:
                 if char in item[0] and char in item[1] and char in item[2]:
                     badges.append(char)
@@ -76,8 +75,8 @@ print(badges)
 # print(final[-1][-1])
 sum_of_badges = []
 for val in badges:
-    i = translation.get(val)
-    sum_of_badges.append(i)
+    x = translation.get(val)
+    sum_of_badges.append(x)
 # print(f" Rucksacks : {len(rucksacks)}")
 # print(f" Groups of 3 : {len(groups_of_3)}")
 # print(f" No_dupes: {len(no_dupes)}")
