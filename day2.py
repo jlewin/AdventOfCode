f7bad8c939a8a10d19d778a8c7e43d8dd669883f
John Lewin <lewin76@gmail.com>
2023-09-20 09:50:36 -0700

    Cleanup day2

--- day2.py
+++ day2.py
@@ -39,25 +39,32 @@ def executeRound(moves):
 
     return winnings + moveScore
 
-
-#plays = plays[0:5]
-#plays = ["C Y"]
-
+# Process all rounds into scores
 scores = [executeRound(play) for play in plays]
 
-
-# test = """A Y
-# B X
-# C Z"""
-
-# scores = [executeRound(l) for l in test.splitlines()]
-
-plays2 = [p.replace('A', 'R').replace('B', 'P').replace('C', 'S')
-          .replace('X', 'R').replace('Y', 'P').replace('Z', 'S') for p in plays]
+# Dump
+print("Total score:", sum(scores))
 
 
-print(plays, "\n", plays2, "\n", scores, "\n", sum(scores))
+# debugging
+#print(scores, "\n", sum(scores))
+# #plays = plays[0:5]
+# #plays = ["C Y"]
 
-executeRound("C Y")
+# scores = [executeRound(play) for play in plays]
 
+
+# # test = """A Y
+# # B X
+# # C Z"""
+
+# # scores = [executeRound(l) for l in test.splitlines()]
+
+# plays2 = [p.replace('A', 'R').replace('B', 'P').replace('C', 'S')
+#           .replace('X', 'R').replace('Y', 'P').replace('Z', 'S') for p in plays]
+
+
+# print(plays, "\n", plays2, "\n", scores, "\n", sum(scores))
+
+# executeRound("C Y")
 
