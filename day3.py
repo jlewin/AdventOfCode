28dbde18b19128141ac5bd17fa860e50113c7faa
John Lewin <lewin76@gmail.com>
2023-09-20 13:25:06 -0700

    Revise day3 loops

--- day3.py
+++ day3.py
@@ -1,8 +1,6 @@
 import string
 
-import trio
-
-input_data = r'C:\Users\Taylor\Downloads\adventofcode.com_2022_day_3_input.txt'
+input_data = r'input/day3.txt'
 
 rucksacks = []
 
@@ -13,8 +11,8 @@ with open(input_data) as my_file:
 
 priorities = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 values = []
-for x in range(1, 53):
-    values.append(x)
+for i in range(1, 53):
+    values.append(i)
 
 translation = {priorities: values for priorities, values in zip(priorities, values)}
 groups_of_3 = []
@@ -22,17 +20,18 @@ group = []
 
 # put elves into groups of 3
 
-for x in range(0, len(rucksacks)):
-    if x == 0:
-        group.append(rucksacks[0])
-    elif x == 299:
-        group.append(rucksacks[299])
-        groups_of_3.append(group.copy())
-    elif x % 3 != 0:
-        group.append(rucksacks[x])
-    elif x % 3 == 0:
+for i in range(0, len(rucksacks)):
+    # Group in intervals of 3
+    if i % 3 == 0:
+        # Append group
         groups_of_3.append(group)
-        group = [rucksacks[x]]
+        group = [rucksacks[i]]
+    else:
+        group.append(rucksacks[i])
+
+# Append final group
+if group not in groups_of_3:
+    groups_of_3.append(group)
 
 no_dupes = []
 
@@ -47,23 +46,24 @@ for badges in groups_of_3:
 sub_total = []
 final = []
 
-for x in range(0, 300):
-    if x == 0:
-        sub_total.append(no_dupes[0])
-    elif x == 299:
-        sub_total.append(no_dupes[299])
-        final.append(sub_total.copy())
-    elif x % 3 != 0:
-        sub_total.append(no_dupes[x])
-    else:
+for i in range(0, len(rucksacks)):
+    if i % 3 == 0 and i != 0:
+        # Append group
         final.append(sub_total)
-        sub_total = [no_dupes[x]]
+        sub_total = [no_dupes[i]]
+    else:
+        sub_total.append(no_dupes[i])
+
+# Append final group
+if sub_total not in final:
+    final.append(sub_total)
+
 
 print(final)
 badges = []
-for x in range(0, 1):
+for i in range(0, 1):
     for item in final:
-        for c in item[x]:
+        for c in item[i]:
             for char in c:
                 if char in item[0] and char in item[1] and char in item[2]:
                     badges.append(char)
@@ -76,8 +76,8 @@ print(badges)
 # print(final[-1][-1])
 sum_of_badges = []
 for val in badges:
-    x = translation.get(val)
-    sum_of_badges.append(x)
+    i = translation.get(val)
+    sum_of_badges.append(i)
 # print(f" Rucksacks : {len(rucksacks)}")
 # print(f" Groups of 3 : {len(groups_of_3)}")
 # print(f" No_dupes: {len(no_dupes)}")
