99e9440abd81c967af4b64d3cd8fbefea63ac741
John Lewin <lewin76@gmail.com>
2023-09-20 16:11:07 -0700

    Add step two of day4

--- day4.py
+++ day4.py
@@ -2,42 +2,39 @@ import string
 
 input_data = r'input/day4.txt'
 
-rucksacks0 = []
+with open(input_data) as my_file:
+    rucksacks = my_file.read().splitlines()
 
-# with open(input_data) as my_file:
-#     contents = my_file.read().splitlines()
-#     for n in contents:
-#         rucksacks.append(n)
-
+# Remap from ordinal indexes in the character set to the target offsets
 def remapToPriority(c):
     pos = ord(c)
     return pos - (96 if ord('a') <= pos <= ord('z') else 38)
 
-rucksacks0 = [
-"vJrwpWtwJgWrhcsFMMfFFhFp",
-"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-"PmmdzqPrVvPwwTWBwg",
-"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-"ttgJtRGJQctTZtZT",
-"CrZsJsPPZsGzwwsLwLmpwMDw" ]
-
 # Split the rucksacks into their compartment values (split at midpoint)
-rucksacks1 = [(s[:mid], s[mid:]) for s in rucksacks0 for mid in [int(len(s) / 2)]]
-#print(rucksacks1)
+compartments = [(s[:mid], s[mid:]) for s in rucksacks for mid in [int(len(s) / 2)]]
+#print(compartments)
 
-# Intersect the two strings to find matching characters between them
-rucksacks2 = [set(c1) & set(c2) for (c1, c2) in rucksacks1]
-#print(rucksacks2)
+# Intersect the two strings to find matching items between them
+shared_item_types = [set(c1) & set(c2) for (c1, c2) in compartments]
+#print(shared_item_types)
 
-# Remap the intersected characters to their prioritys
-rucksacks3 = [remapToPriority(c) for s in rucksacks2 for c in s ]
-#print(rucksacks3)
+# Remap the intersected characters to their priorities
+item_priorities = [remapToPriority(c) for s in shared_item_types for c in s ]
+#print(item_priorities)
 
-# Sum the priorities
-print(sum(rucksacks3))
+# Sum the item priorities
+print("Sum of item priorities:", sum(item_priorities))
 
+# Find badges in each 3 Elven group
+badges = []
+for i in range(0, len(rucksacks), 3):
+    matched = set(rucksacks[i]) & set(rucksacks[i+1]) & set(rucksacks[i+2])
+    assert (len(matched) == 1, 'x must be positive')
+    badges.append(list(matched)[0])
+#print(badges)
 
+badge_priorities = [remapToPriority(b) for b in badges]
+#print(badge_priorities)
 
-# strings = ['hello', 'world22']
-# modified = [s_mod for s in strings for s_mod in [len(s)]]
-# print(modified) 
+# Sum the badge priorities
+print("Sum of badge priorites:", sum(badge_priorities))
