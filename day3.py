dde1abca3a68303e6bc18eb9566e77384bb0deb2
John Lewin <lewin76@gmail.com>
2023-09-20 20:20:20 -0700

    Fix day naming

--- day3.py
+++ day3.py
@@ -1,86 +1,38 @@
-import string
-
 input_data = r'input/day3.txt'
 
-rucksacks = []
-
 with open(input_data) as my_file:
-    contents = my_file.read().splitlines()
-    for n in contents:
-        rucksacks.append(n)
+    rucksacks = my_file.read().splitlines()
 
-priorities = list(string.ascii_lowercase) + list(string.ascii_uppercase)
-values = []
-for x in range(1, 53):
-    values.append(x)
+# Remap from ordinal indexes in the character set to the target offsets
+def remapToPriority(c):
+    pos = ord(c)
+    return pos - (96 if ord('a') <= pos <= ord('z') else 38)
 
-translation = {priorities: values for priorities, values in zip(priorities, values)}
-groups_of_3 = []
-group = []
+# Split the rucksacks into their compartment values (split at midpoint)
+compartments = [(s[:mid], s[mid:]) for s in rucksacks for mid in [int(len(s) / 2)]]
+#print(compartments)
 
-# Put into groups of 3
-for x in range(0, len(rucksacks)):
-    # Group in intervals of 3
-    if x % 3 == 0:
-        # Append group
-        groups_of_3.append(group)
-        group = [rucksacks[x]]
-    else:
-        group.append(rucksacks[x])
+# Intersect the two strings to find matching items between them
+shared_item_types = [set(c1) & set(c2) for (c1, c2) in compartments]
+#print(shared_item_types)
 
-# Append final group
-if group not in groups_of_3:
-    groups_of_3.append(group)
+# Remap the intersected characters to their priorities
+item_priorities = [remapToPriority(c) for s in shared_item_types for c in s ]
+#print(item_priorities)
 
-no_dupes = []
+# Sum the item priorities
+print("Sum of item priorities:", sum(item_priorities))
 
-# remove duplicates from each elf pack
-
-for badges in groups_of_3:
-    for line in badges:
-        a = sorted(set(line))
-        no_dupes.append(a)
-
-# put sets back in groups of 3
-sub_total = []
-final = []
-
-for x in range(0, len(rucksacks)):
-    if x % 3 == 0 and x != 0:
-        # Append group
-        final.append(sub_total)
-        sub_total = [no_dupes[x]]
-    else:
-        sub_total.append(no_dupes[x])
-
-# Append final group
-if sub_total not in final:
-    final.append(sub_total)
-
-
-print(final)
+# Find badges in each 3 Elven group
 badges = []
-for x in range(0, 1):
-    for item in final:
-        for c in item[x]:
-            for char in c:
-                if char in item[0] and char in item[1] and char in item[2]:
-                    badges.append(char)
+for i in range(0, len(rucksacks), 3):
+    matched = set(rucksacks[i]) & set(rucksacks[i+1]) & set(rucksacks[i+2])
+    assert (len(matched) == 1, 'x must be positive')
+    badges.append(list(matched)[0])
+#print(badges)
 
-print(badges)
-# print(rucksacks[-1])
-# print(groups_of_3[-1])
-# print(no_dupes[-1])
-# print(final[-1])
-# print(final[-1][-1])
-sum_of_badges = []
-for val in badges:
-    x = translation.get(val)
-    sum_of_badges.append(x)
-# print(f" Rucksacks : {len(rucksacks)}")
-# print(f" Groups of 3 : {len(groups_of_3)}")
-# print(f" No_dupes: {len(no_dupes)}")
-# print(f" Final: {len(final)}")
+badge_priorities = [remapToPriority(b) for b in badges]
+#print(badge_priorities)
 
-print(sum_of_badges)
-print(sum(sum_of_badges))
+# Sum the badge priorities
+print("Sum of badge priorites:", sum(badge_priorities))
