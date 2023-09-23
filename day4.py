f2456c4e3c697c29b09042762cbc4f03ce84aea7
John Lewin <lewin76@gmail.com>
2023-09-23 09:13:06 -0700

    Add day4

---
+++ day4.py
@@ -0,0 +1,26 @@
+
+with open('input/day4.txt') as infile:
+    lines = infile.read().splitlines()
+
+def assignments_fully_overlap(elve_pair):
+    #print(elve_pair)
+    section_assigments = [range(int(lower), int(upper)+1)
+                for r in elve_pair.split(",")
+                    for lower, upper in [r.split('-')]]
+
+    a = set(section_assigments[0])
+    b = set(section_assigments[1])
+
+    # Part 1
+    #return a.issubset(b) or b.issubset(a)
+
+    # Part 2
+    return a & b
+
+# Find overlapping assignments
+overlap = [assignments_fully_overlap(l) for l in lines]
+
+# Count pairs with overlap
+count_overlapped = len([b for b in overlap if b])
+print("Count overlapped:", count_overlapped)
+#print("Results", overlap)
