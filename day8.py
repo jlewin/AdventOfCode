e815eab1fd38af1e80ebe215fdb559f2dd7ee26a
John Lewin <lewin76@gmail.com>
2023-09-25 13:33:33 -0700

    Add day8 part2

--- day8.py
+++ day8.py
@@ -1,12 +1,14 @@
+from itertools import takewhile
 
 with open('input/day8.txt') as infile:
     lines = infile.read().splitlines()
 
-trees = [list(l) for l in lines]
-visible_trees = []
-
+# Covert each line into list of ints, in a list of lists
+trees = [[int(char) for char in l] for l in lines]
 rows = len(trees)
 cols = len(trees[0])
+
+max_score = 0
 
 # Loop over rows and columns
 for r in range(rows):
@@ -15,9 +17,7 @@ for r in range(rows):
         row = trees[r]
         col = [trees[i][c] for i in range(rows)]
 
-        # This tree's height
-        height = row[c]
-
+        height = row[c]  # This tree's height
         next_column = c + 1
         next_row = r + 1
 
@@ -27,20 +27,47 @@ for r in range(rows):
         right = row[next_column:]
         down = col[next_row:]
 
-        # Push visibility results into tuple for improved debugging
-        visibility = [
-            ('up', len(up) == 0 or height > max(up)),
-            ('left', len(left) == 0 or height > max(left)),
-            ('right', len(right) == 0  or height > max(right)),
-            ('down', len(down) == 0 or height > max(down))]
+        # Part 2
+        def visible_trees_distance(items):
+            visible = []
+            distance = 0
+            # Loop over the list items, collecting until threshold is hit
+            for h in items:
+                distance = distance + 1
+                visible.append((distance, h))
+                if h >= height:
+                    break;
+            return distance
 
-        # Consider all four direcions for tree visibility
-        is_visible = any([v[1] for v in visibility])
-        visible_trees.append(is_visible)
+            #result =  list(takewhile(lambda sibling_height: sibling_height < height, rev))
 
-        # Debugging helper
-        #print(f"({r}, {c})", "\tVisible", is_visible, visibility)
+        # up, left, right, down
+        scores = [
+            visible_trees_distance(reversed(up)),
+            visible_trees_distance(reversed(left)),
+            visible_trees_distance(right),
+            visible_trees_distance(down),
+        ]
 
-# Print results
-print("Visible trees", sum(visible_trees))
+        scenic_score = scores[0] * scores[1] * scores[2] * scores[3]
+        max_score = max(max_score, scenic_score)
+
+print("Maximum score", max_score)
+
+def part1():
+    # Push visibility results into tuple for improved debugging
+    visibility = [
+        ('up', len(up) == 0 or height > max(up)),
+        ('left', len(left) == 0 or height > max(left)),
+        ('right', len(right) == 0  or height > max(right)),
+        ('down', len(down) == 0 or height > max(down))]
+
+    # Consider all four directions for tree visibility
+    is_visible = any([v[1] for v in visibility])
+    visible_trees_distance.append(is_visible)
+
+    # Debugging helper
+    print(f"({r}, {c})", "\tVisible", is_visible, visibility)
+
+
 
